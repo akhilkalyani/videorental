9eef8e3aa2397a8e91996a2ec545377900dcc8e6
deepak-kalyani <akhilkalyani7754@gmail.com>
2025-01-25 11:08:31 +0530

    Removed unnecessary imports from AdminController

--- src/main/java/com/crio/videorental/controller/AdminController.java
+++ src/main/java/com/crio/videorental/controller/AdminController.java
@@ -4,7 +4,6 @@ import com.crio.videorental.model.Video;
 import com.crio.videorental.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
